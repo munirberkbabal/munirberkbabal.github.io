910b2a22f95709dcd2ec663af4a7eb201316e394
munirberkbabal <73224930+munirberkbabal@users.noreply.github.com>
2024-12-31 00:04:05 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
-function goToApp() {
-  const select = document.getElementById('app-select');
-  const selectedApp = select.value;
-  if (selectedApp) {
-    window.location.href = selectedApp;
+// Alt menüyü açıp kapatmak için
+const mobileAppsBtn = document.getElementById('mobile-apps-btn');
+const mobileAppsMenu = document.getElementById('mobile-apps-menu');
+
+mobileAppsBtn.addEventListener('click', (e) => {
+  e.preventDefault(); // Sayfanın yenilenmesini engelle
+  mobileAppsMenu.style.display = mobileAppsMenu.style.display === 'block' ? 'none' : 'block';
+});
+
+// Sayfanın herhangi bir yerine tıklandığında menüyü kapat
+document.addEventListener('click', (e) => {
+  if (!e.target.closest('.dropdown')) {
+    mobileAppsMenu.style.display = 'none';
   }
-}
+});
